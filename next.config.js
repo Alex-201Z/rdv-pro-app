1683cda5fc2aca615e2a9fbe5d435f82bf8f78e5
alex-2017 <ondoalex2020@gmail.com>
2026-01-04 18:47:56 +0100

    chore: add no-cache headers to fix chunk load error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // rewrites removed for Supabase Serverless migration
+  // Force no caching for deployment debugging
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
